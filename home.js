2a39756d15106242a68730474ec00f7f228d5817
gorret <nabatanzigorret143@gmail.com>
2022-02-15 22:24:59 +0300

    adding react to website

---
+++ home.js
@@ -0,0 +1,16 @@
+function Home(){
+    return(
+        <>
+         <h1>Adding React to a webpage </h1>
+         <img  src="logo.png" />
+        <h3>by</h3>
+        <h2 class="name">Nabatanzi Gorret</h2>
+        
+        </>
+       
+
+
+    )
+}
+
+  
